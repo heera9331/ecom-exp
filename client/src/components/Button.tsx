7c8ac272a82332a5dabd8b55e9399c407f15df47
heera9331 <heera9331@gmail.com>
2024-02-20 04:47:55 +0530

    button cmp changed

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,7 +1,11 @@
-
+import React from "react";
 // className is optional
 
-const Button = ({ text, className, onClick, ...props }: { text: string, className?: string, onClick: () => void }) => {
+interface buttonProps {
+    text: string, className?: string, onClick: () => void
+}
+
+const Button: React.FC<buttonProps> = ({ text, className, onClick, ...props }: buttonProps) => {
 
     return (
         <button
