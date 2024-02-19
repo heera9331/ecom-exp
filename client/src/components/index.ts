7e2143f2b707541226eea4324938ec168953b12c
heera9331 <heera9331@gmail.com>
2024-02-20 05:20:42 +0530

    header, footer file created and exported

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,5 +1,7 @@
 import Button from "./Button";
 import Input from "./Input";
 import TextArea from "./TextArea";
+import Header from './Header/Header';
+import Footer from "./Footer/Footer";
 
-export { Button, Input, TextArea };
+export { Button, Input, TextArea, Header, Footer};
