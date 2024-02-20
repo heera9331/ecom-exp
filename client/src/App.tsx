32f76dece230ef4f9bbdc50d1460696bd792fc7f
heera9331 <heera9331@gmail.com>
2024-02-20 05:34:28 +0530

    cart page added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Login, Register, About, Contact } from './pages';
