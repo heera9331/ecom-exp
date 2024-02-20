bfc9fa125c2717aea06f37642114fed5e5afaac5
heera9331 <heera9331@gmail.com>
2024-02-20 05:47:55 +0530

    litter style of header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -4,10 +4,14 @@ const Header = () => {
   const navigate = useNavigate();
 
   return (
-    <header>
-      <h1>Logo</h1>
-      <input type="search" placeholder="Search here..." />
-      <ul>
+    <header className="flex items-center justify-between bg-gray-100 p-2 border-b border-black border-opacity-25 rounded-sm">
+      <h1 className="text-3xl font-semibold">Logo</h1>
+      <input
+        type="search"
+        placeholder="Search here..."
+        className="py-1 px-2 border border-black border-opacity-25 rounded-sm"
+      />
+      <ul className="flex items-center justify-center gap-2">
         <li>
           <Link to={'/login'}>Login</Link>
         </li>
