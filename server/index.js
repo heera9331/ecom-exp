1c72af6167e9e480cf0cb05454714c33a204adf8
Yogesh <chauhanyogesh950@gmail.com>
2024-02-21 22:18:55 +0530

    .

--- server/index.js
+++ server/index.js
@@ -5,10 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 const PORT = process.env.PORT || 8000;
-<<<<<<< HEAD
-=======
 const router = express.Router();
->>>>>>> b73a3724a7cd5fffd90296567f7b481772a1c407
 const app = express();
 
 
