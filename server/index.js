457c8727b8241d7b49a2cc82fbcfa6ad69d5a4a8
heera9331 <heera9331@gmail.com>
2024-02-21 22:07:51 +0530

    db connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
+import connectDB from "./utils/db.js";
 
 import dotenv from "dotenv";
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -11,4 +13,5 @@ app.get("/", async (req, res, next) => {
 
 app.listen(PORT, () => {
   console.log("server has started on port", PORT);
+  connectDB();
 });
