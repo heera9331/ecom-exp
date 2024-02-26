ca7dd60f5759b44cc87072026b18c7795ed57f7e
heera9331 <heera9331@gmail.com>
2024-02-26 14:42:49 +0530

    update product controller

--- next-app/src/app/api/products/route.ts
+++ next-app/src/app/api/products/route.ts
@@ -26,3 +26,25 @@ export const POST = async (req: NextRequest, res: NextResponse) => {
 
     return new NextRequest({ ack });
 }
+
+export const UPDATE = async (req: NextRequest, res: NextResponse) => {
+    req = await req.json();
+    let updated = req.product;
+    let id = req.id;
+
+    if (!product) {
+        return new NextResponse({ error: "product not found that will be update" });
+    }
+
+    if (!id) {
+        return new NextResponse({ error: "product id not found that will be update" });
+    }
+
+    let ack = await Product.updateOne({}, { updated })
+
+    if (ack) {
+        return new NextResponse({ ack });
+    }
+
+    return new NextResponse.json({ error: "database error" });
+}
