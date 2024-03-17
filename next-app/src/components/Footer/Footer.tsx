1b30c0c260c542d5952251a34d30ea35668324f3
heera9331 <heera9331@gmail.com>
2024-03-17 22:18:19 +0530

    header and footer changed

--- next-app/src/components/Footer/Footer.tsx
+++ next-app/src/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@
 
 const Footer = () => {
     return (
-        <div className="flex items-center justify-center bg-gray-100 p-2 border-t border-black border-opacity-25 rounded-sm h-[100px]">
-            <h1 className="text-center font-semibold text-xl text-gray-800">Design & Developed By - Yogesh && Heera</h1>
+        <div className="flex items-center justify-center bg-gray-900  p-2 border-t border-black border-opacity-25 rounded-sm h-[100px]">
+            <h1 className="text-center font-semibold text-xl text-gray-100">Design & Developed By - Yogesh && Heera</h1>
         </div>
     )
 }
