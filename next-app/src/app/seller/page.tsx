16e20c2cbfd5fd1d383f1905b6e0a480bda48c1a
heera9331 <heera9331@gmail.com>
2024-03-21 13:09:08 +0530

    link changed

--- next-app/src/app/seller/page.tsx
+++ next-app/src/app/seller/page.tsx
@@ -4,8 +4,9 @@ function Page() {
     return (
         <div>
             <h1 className="text-2xl font-semibold">Seller Page</h1>
-            <Link href={'/seller/add-product'}>1. Add Product</Link>
-            <h2>2. seller products display here...</h2>
+            <div className="flex flex-col gap-1 py-2">
+                <Link href={'/seller/add-product'} className="text-blue-800 underline font-semibold">1. Add Product</Link>
+                <h2>2. seller products display here...</h2></div>
         </div>
     );
 }
