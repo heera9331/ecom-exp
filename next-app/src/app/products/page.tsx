408d425e2f70b38afa14eb6a04baa9a814c32b79
heera9331 <heera9331@gmail.com>
2024-03-21 13:56:50 +0530

    designing product page

--- next-app/src/app/products/page.tsx
+++ next-app/src/app/products/page.tsx
@@ -1,10 +1,10 @@
 import Card, { Product } from "@/components/Card/Card";
 import axios from "axios";
 
-const fetchData = async ()=>{
+const fetchData = async () => {
   const response = await axios.get('http://localhost:3000/api/products')
   console.log(response)
-  const data =  response.data
+  const data = response.data
   return data.products
 }
 
@@ -15,15 +15,20 @@ export default async function Home() {
 
   return (
     <div>
-      <div className="text-center">
-        <h1>
-          Our Products
-        </h1>
-      </div>
+      <h1 className="font-semibold text-2xl">Our Products</h1>
       <div className=" flex justify-center items-center py-5 border-2 border-blue-500">
-        
         <div className="w-4/5 m-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 border-2 border-red-500">
-          {dataArray.map((item:Product)=> <div key={item._id}><Card _id={item._id} imageUrl={item.imageUrl} buyPrice={item.buyPrice} name={item.name} description={item.description} msrp={item.msrp}/></div>)}
+          {dataArray && dataArray.map((item: Product) =>
+            <div key={item._id}>
+              {/* render card when image url avaiable */}
+              <Card _id={item._id}
+                imageUrl={item.imageUrl}
+                buyPrice={item.buyPrice}
+                name={item.name}
+                description={item.description}
+                msrp={item.msrp} />
+            </div>
+          )}
         </div>
       </div>
     </div>
