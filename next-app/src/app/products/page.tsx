9536ebca7592d6f5da5564bcb72d42745601496d
heera9331 <heera9331@gmail.com>
2024-04-06 22:06:41 +0530

    categories added

--- next-app/src/app/products/page.tsx
+++ next-app/src/app/products/page.tsx
@@ -1,6 +1,8 @@
 import Card, { Product } from "@/components/Card/Card";
 import axios from "axios";
 
+const categories = ["Clothes", "Tshirt", "Shirt", "Jogger", "Accessories"]
+
 const fetchData = async () => {
   const response = await axios.get('http://localhost:3000/api/products')
   console.log(response)
@@ -31,6 +33,31 @@ export default async function Home() {
           )}
         </div>
       </div>
+      <hr />
+
+      <section className="my-10 py-4 bg-sky-200/50">
+        <div className="flex items-center flex-col">
+          <h2 className="font-semibold text-2xl my-1">Browse Products by Categories</h2>        
+          <div className="bg-sky-500 h-[2px] w-[200px] "></div>
+        </div>
+        
+        
+        <div className="my-4 flex gap-3 flex-wrap items-center justify-center">
+          {categories.map((category:any, idx:number)=>{
+            return <a href={`products/${category}`} key={idx}>   
+            <div key={idx} className="w-[250px] h-[250px] shadow-md hover:shadow-lg bg-white p-2">
+              {/* category image */}
+              <div className=""></div>
+              {/* category title */}
+              <h2 className="text-xl text-center font-semibold">{category}</h2>
+            </div>
+            </a>
+          })}
+        </div>
+      </section>
+
+      
+      <hr />
     </div>
   );
 }
