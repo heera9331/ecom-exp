38805f9f1c14739c89ae0ca9e702f0ff1063f6c0
heera9331 <heera9331@gmail.com>
2024-04-18 21:49:08 +0530

    react app removed

--- client/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-import Button from "./Button";
-import Input from "./Input";
-import TextArea from "./TextArea";
-import Header from './Header/Header';
-import Footer from "./Footer/Footer";
-
-export { Button, Input, TextArea, Header, Footer};
